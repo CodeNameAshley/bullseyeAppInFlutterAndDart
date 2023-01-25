7a289f8a4e7afd3749929e066b9cc37f425f6b01
Ashley <ashleynavo@googlemail.com>
2023-01-25 22:55:28 +0000

    Start of Project

---
+++ lib/main.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() {
+  runApp(const BullEyeApp());
+}
+
+class BullEyeApp extends StatelessWidget {
+  const BullEyeApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.landscapeLeft,
+      DeviceOrientation.landscapeRight,
+    ]);
+    return const MaterialApp(
+      title: 'Bullseye',
+      home: GamePage(),
+    );
+  }
+}
+
+class GamePage extends StatefulWidget {
+  const GamePage({super.key});
+
+  @override
+  State<GamePage> createState() => _GamePageState();
+}
+
+class _GamePageState extends State<GamePage> {
+  bool _alertIsVisible = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          const Text(
+            'Hello Bullseye',
+            style: TextStyle(fontWeight: FontWeight.bold, color: Colors.green),
+          ),
+          TextButton(
+            onPressed: () {
+              _alertIsVisible = true;
+              _showAlert(context);
+            },
+            child: const Text(
+              'Hit Me!',
+              style: TextStyle(color: Colors.blue),
+            ),
+          )
+        ],
+      ),
+    ));
+  }
+
+  void _showAlert(BuildContext context) {
+    var okButton = TextButton(
+      onPressed: () {
+        Navigator.of(context).pop();
+        _alertIsVisible = false;
+        print('Awesome pressed! $_alertIsVisible');
+      },
+      child: const Text('Awesome!'),
+    );
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Hello there!'),
+          content: const Text('This is my first pop-up!'),
+          actions: [
+            okButton,
+          ],
+          elevation: 5,
+        );
+      },
+    );
+  }
+}
